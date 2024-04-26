6e6be512ad017498481b80d43e6df5918dd760ee
cybernekocadavre <124934209+cybernekocadavre@users.noreply.github.com>
2024-04-26 17:53:53 +0300

    Update client.py

--- client.py
+++ client.py
@@ -7,7 +7,7 @@
 import asyncio
 
 async def tcp_echo_client(message):
-    reader, writer = await asyncio.open_connection('127.0.0.1', 8888)
+    reader, writer = await asyncio.open_connection('127.0.0.1', 8889)
     
     writer.write(message.encode())
     await writer.drain()
